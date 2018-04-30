9ded27c1afd228d3ca7fa75adf7de8f5c0262789
vinbdias <vintheman@gmail.com>
2018-04-30 09:21:09 -0300

    Correções nos cálculos de IR cobrado erroneamente. Aguarda homologação.

--- js/app/models/Simulacao.js
+++ js/app/models/Simulacao.js
@@ -60,45 +60,35 @@ class Simulacao {
                     aliquotaQuartoPisoQuartoTeto: '0.225', //22,5%
                     aliquotaQuintoPiso: '0.275' //27,5%
                 }
-            },
-            //=SE($C$6<=(1164*$C$8)-0;($C$6*0)-0*$C$8;SE($C$6<=2326*$C$8;SE($C$6>=1164,01*$C$8;($C$6*0,15)-174,6*$C$8;0);SE($C$6>=2326,01*$C$8;($C$6*0,275)-465,35*$C$8;0)))
-            //=SE($C$6<=(1164*1)-0;($C$6*0)-0*1;SE($C$6<=2326*1;SE($C$6>=1164,01*1;($C$6*0,15)-174,6*1;0);SE($C$6>=2326,01*1;($C$6*0,275)-465,35*1;0)))
+            },            
             2005: {
                 taxaSelic: '1.2745', //127,45%
                 primeiroTeto: '1164.00',//ISENTO
                 segundoTeto: '2326.00',
                 valorADeduzirSegundoPisoSegundoTeto: '174.60',
                 valorADeduzirTerceiroPisoTerceiroTeto: '465.35'
-            },
-            // =SE($C$6<=(1257,12*$C$8)-0;($C$6*0)-0*$C$8;SE($C$6<=2512,08*$C$8;SE($C$6>=1257,13*$C$8;($C$6*0,15)-188,57*$C$8;0);SE($C$6>=2512,09*$C$8;($C$6*0,275)-502,58*$C$8;0)))
-            //=SE($C$6<=(1257,12*1)-0;($C$6*0)-0*1;SE($C$6<=2512,08*1;SE($C$6>=1257,13*1;($C$6*0,15)-188,57*1;0);SE($C$6>=2512,09*1;($C$6*0,275)-502,58*1;0)))
+            },                    
             2006: {
                 taxaSelic: '1.1432', //114,32%
                 primeiroTeto: '1257.12',//ISENTO
                 segundoTeto: '2512.08',
                 valorADeduzirSegundoPisoSegundoTeto: '188.57',
                 valorADeduzirTerceiroPisoTerceiroTeto: '502.58'
-            },
-            //=SE($C$6<=(1313,69*$C$8)-0;($C$6*0)-0*$C$8;SE($C$6<=2625,12*$C$8;SE($C$6>=1313,7*$C$8;($C$6*0,15)-197,05*$C$8;0);SE($C$6>=2652,13*$C$8;($C$6*0,275)-525,19*$C$8;0)))
-            //=SE($C$6<=(1313,69*1)-0;($C$6*0)-0*1;SE($C$6<=2625,12*1;SE($C$6>=1313,7*1;($C$6*0,15)-197,05*1;0);SE($C$6>=2652,13*1;($C$6*0,275)-525,19*1;0)))
+            },            
             2007: {
                 taxaSelic: '1.0350', //103,50%
                 primeiroTeto: '1313.69',//ISENTO
                 segundoTeto: '2625.12',
                 valorADeduzirSegundoPisoSegundoTeto: '197.05',
                 valorADeduzirTerceiroPisoTerceiroTeto: '525.19'
-            },
-            //=SE($C$6<=(1372,81*$C$8)-0;($C$6*0)-0*$C$8;SE($C$6<=2743,25*$C$8;SE($C$6>=1372,82*$C$8;($C$6*0,15)-205,92*$C$8;0);SE($C$6>=2743,26*$C$8;($C$6*0,275)-548,82*$C$8;0)))
-            //=SE($C$6<=(1372,81*1)-0;($C$6*0)-0*1;SE($C$6<=2743,25*1;SE($C$6>=1372,82*1;($C$6*0,15)-205,92*1;0);SE($C$6>=2743,26*1;($C$6*0,275)-548,82*1;0)))
+            },                    
             2008: {
                 taxaSelic: '0.9137', //91,37%
                 primeiroTeto: '1372.81',//ISENTO
                 segundoTeto: '2743.25',
                 valorADeduzirSegundoPisoSegundoTeto: '205.92',
                 valorADeduzirTerceiroPisoTerceiroTeto: '548.82'
-            },
-            //=SE($C$6<=(1434,59*$C$8)-0;($C$6*0)-0*$C$8;SE($C$6<=2150*$C$8;SE($C$6>=1434,6*$C$8;($C$6*0,075)-107,59*$C$8;0);SE($C$6<=2866,7*$C$8;SE($C$6>=2150,01*$C$8;($C$6*0,15)-268,84*$C$8;0);SE($C$6>=2866,71*$C$8;SE($C$6<=3582*$C$8;($C$6*0,225)-483,84*$C$8;SE($C$6>=3582,01*$C$8;($C$6*0,275)-662,94*$C$8;0))))))
-            //=SE($C$6<=(1434,59*1)-0;($C$6*0)-0*1;SE($C$6<=2150*1;SE($C$6>=1434,6*1;($C$6*0,075)-107,59*1;0);SE($C$6<=2866,7*1;SE($C$6>=2150,01*1;($C$6*0,15)-268,84*1;0);SE($C$6>=2866,71*1;SE($C$6<=3582*1;($C$6*0,225)-483,84*1;SE($C$6>=3582,01*1;($C$6*0,275)-662,94*1;0))))))
+            },                    
             2009: {
                 taxaSelic: '0.8274', //82.74%
                 primeiroTeto: '1434.59',//ISENTO
@@ -109,9 +99,7 @@ class Simulacao {
                 valorADeduzirTerceiroPisoTerceiroTeto: '268.84',
                 valorADeduzirQuartoPisoQuartoTeto: '483.84',
                 valorADeduzirQuintoPiso: '662.94'
-            },
-            //=SE($C$6<=(1499,15*$C$8)-0;($C$6*0)-0*$C$8;SE($C$6<=2246,75*$C$8;SE($C$6>=1499,16*$C$8;($C$6*0,075)-112,43*$C$8;0);SE($C$6<=2995,7*$C$8;SE($C$6>=2246,76*$C$8;($C$6*0,15)-280,94*$C$8;0);SE($C$6>=2995,71*$C$8;SE($C$6<=3743,19*$C$8;($C$6*0,225)-505,62*$C$8;SE($C$6>=3743,2*$C$8;($C$6*0,275)-692,78*$C$8;0))))))
-            //=SE($C$6<=(1499,15*1)-0;($C$6*0)-0*1;SE($C$6<=2246,75*1;SE($C$6>=1499,16*1;($C$6*0,075)-112,43*1;0);SE($C$6<=2995,7*1;SE($C$6>=2246,76*1;($C$6*0,15)-280,94*1;0);SE($C$6>=2995,71*1;SE($C$6<=3743,19*1;($C$6*0,225)-505,62*1;SE($C$6>=3743,2*1;($C$6*0,275)-692,78*1;0))))))
+            },                        
             2010: {
                 taxaSelic: '0.7276', //72,76%
                 primeiroTeto: '1499.15',//ISENTO
@@ -122,9 +110,7 @@ class Simulacao {
                 valorADeduzirTerceiroPisoTerceiroTeto: '280.94',
                 valorADeduzirQuartoPisoQuartoTeto: '505.62',
                 valorADeduzirQuintoPiso: '692.78'
-            },
-            //=SE($C$6<=(1566,61*$C$8)-0;($C$6*0)-0*$C$8;SE($C$6<=2347,85*$C$8;SE($C$6>=1566,62*$C$8;($C$6*0,075)-117,49*$C$8;0);SE($C$6<=3130,51*$C$8;SE($C$6>=2347,86*$C$8;($C$6*0,15)-293,58*$C$8;0);SE($C$6>=3130,52*$C$8;SE($C$6<=3911,63*$C$8;($C$6*0,225)-528,37*$C$8;SE($C$6>=3911,64*$C$8;($C$6*0,275)-723,95*$C$8;0))))))
-            //=SE($C$6<=(1566,61*1)-0;($C$6*0)-0*1;SE($C$6<=2347,85*1;SE($C$6>=1566,62*1;($C$6*0,075)-117,49*1;0);SE($C$6<=3130,51*1;SE($C$6>=2347,86*1;($C$6*0,15)-293,58*1;0);SE($C$6>=3130,52*1;SE($C$6<=3911,63*1;($C$6*0,225)-528,37*1;SE($C$6>=3911,64*1;($C$6*0,275)-723,95*1;0))))))
+            },                    
             2011: {
                 taxaSelic: '0.6188', //61,68%
                 primeiroTeto: '1566.61',//ISENTO
@@ -342,7 +328,7 @@ class Simulacao {
 
     get aliquotaQuintoPiso() {
 
-        return this._aliquotaQuintoPisoQuintoTeto;
+        return this._aliquotaQuintoPiso;
     }
 
     get irDevido() {
@@ -399,30 +385,25 @@ class Simulacao {
     _calculoAPartirDe2009() {
 
         let irDevido = 0.00;
-        let irCobrado = 0.00;
-        //=SE($C$6<=(1434,59*$C$8)-0;($C$6*0)-0*$C$8;SE($C$6<=2150*$C$8;SE($C$6>=1434,6*$C$8;($C$6*0,075)-107,59*$C$8;0);SE($C$6<=2866,7*$C$8;SE($C$6>=2150,01*$C$8;($C$6*0,15)-268,84*$C$8;0);SE($C$6>=2866,71*$C$8;SE($C$6<=3582*$C$8;($C$6*0,225)-483,84*$C$8;SE($C$6>=3582,01*$C$8;($C$6*0,275)-662,94*$C$8;0))))))
-        //=SE($C$6<=(1434,59*1)-0;($C$6*0)-0*1;SE($C$6<=2150*1;SE($C$6>=1434,6*1;($C$6*0,075)-107,59*1;0);SE($C$6<=2866,7*1;SE($C$6>=2150,01*1;($C$6*0,15)-268,84*1;0);SE($C$6>=2866,71*1;SE($C$6<=3582*1;($C$6*0,225)-483,84*1;SE($C$6>=3582,01*1;($C$6*0,275)-662,94*1;0))))))
+        let irCobrado = 0.00;        
+        
         if((this._primeiroTeto * this._numeroDeMeses) < this._baseDeCalculo && this._baseDeCalculo <= (this._segundoTeto * this._numeroDeMeses)) {
-
-            //alert('1');
+            
             irDevido = (this._baseDeCalculo * this._aliquotaSegundoPisoSegundoTeto) - (this._valorADeduzirSegundoPisoSegundoTeto * this._numeroDeMeses);
-            irCobrado = (this._baseDeCalculo * this._aliquotaSegundoPisoSegundoTeto) - this._valorADeduzirSegundoPisoSegundoTeto;
+            irCobrado = (this._baseDeCalculo * this._aliquotaQuintoPiso) - this._valorADeduzirQuintoPiso;
         }
         else if((this._segundoTeto * this._numeroDeMeses) < this._baseDeCalculo && this._baseDeCalculo <= (this._terceiroTeto * this._numeroDeMeses)) {
-
-            //alert('2');
+            
             irDevido = (this._baseDeCalculo * this._aliquotaTerceiroPisoTerceiroTeto) - (this._valorADeduzirTerceiroPisoTerceiroTeto * this._numeroDeMeses);
-            irCobrado = (this._baseDeCalculo * this._aliquotaQuintoPisoQuintoTeto) - this._valorADeduzirQuintoPiso;
+            irCobrado = (this._baseDeCalculo * this._aliquotaQuintoPiso) - this._valorADeduzirQuintoPiso;
         }
         else if((this._terceiroTeto * this._numeroDeMeses) < this._baseDeCalculo && this._baseDeCalculo <= (this._quartoTeto * this._numeroDeMeses)) {
-
-            //alert('3');
+            
             irDevido = (this._baseDeCalculo * this._aliquotaQuartoPisoQuartoTeto) - (this._valorADeduzirQuartoPisoQuartoTeto * this._numeroDeMeses);
-            irCobrado = (this._baseDeCalculo * this._aliquotaQuartoPisoQuartoTeto) - this._valorADeduzirQuartoPisoQuartoTeto;
+            irCobrado = (this._baseDeCalculo * this._aliquotaQuintoPiso) - this._valorADeduzirQuintoPiso;
         }
         else if((this._quartoTeto * this._numeroDeMeses) < this._baseDeCalculo) {
 
-            //alert('4');
             irDevido = (this._baseDeCalculo * this._aliquotaQuintoPiso) - (this._valorADeduzirQuintoPiso * this._numeroDeMeses);
             irCobrado = (this._baseDeCalculo * this._aliquotaQuintoPiso) - this._valorADeduzirQuintoPiso;
         }
